77bf853d41c489af9d373bb21562f6a7d77be706
Sean Behan <inbox@seanbehan.com>
2021-05-10 20:39:58 -0400

    .

---
+++ host/index.jsx
@@ -0,0 +1,7 @@
+/**
+  Here is where you use the Adobe API
+*/
+function openDocument(){
+  var fileRef = new File("/Users/sean/ImageRelay/Assets/Photos/hoodh-ahmed-1325762-unsplash.jpg");
+  var docRef = app.open(fileRef);
+}
